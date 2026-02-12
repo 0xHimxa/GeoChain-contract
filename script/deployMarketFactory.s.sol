b1223778cb6364521bdb6e809c8a359b780a89f5
himxa <himxa0x@gmail.com>
2026-02-12 19:28:35 +0100

    added sepecific pragma solidiy version

---
+++ script/deployMarketFactory.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.33;
+
+import {Script} from "forge-std/Script.sol";
+import {MarketFactory} from "src/MarketFactory.sol";
+import {PredictionMarket} from "src/PredictionMarket.sol";
+import {ERC20MOCK} from ""
+
+contract DeployMarketFactory is Script{
+
+
+
+
+
+
+
+
+
+
+function getCollateralByChainId()  internal{
+ address
+
+if(block.chainid == 5511555){
+    return
+} 
+else{
+
+
+
+
+}
+
+
+
+
+}
+
+}
