14a08659540e0724678ef0a6629d3add03775ea3
himxa <himxa0x@gmail.com>
2026-02-26 15:55:47 +0100

    remodified my vault to insherit from reciver and modie  my frotned

--- contract/src/router/PredictionMarketRouterVault.sol
+++ contract/src/router/PredictionMarketRouterVault.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.33;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReceiverTemplate} from "../../script/interfaces/ReceiverTemplate.sol";
 
 interface IPredictionMarketLike {
     function i_collateral() external view returns (address);
@@ -24,7 +24,7 @@ interface IPredictionMarketLike {
 /// @notice Router vault that enables one-time collateral approval UX while handling market interactions on behalf of users.
 /// @dev Users deposit collateral once to this vault and then trade through internal balances.
 /// Market contracts see this vault as trader/liquidity provider, which avoids repeated per-market approvals.
-contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
+contract PredictionMarketRouterVault is ReceiverTemplate, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     error Router__ZeroAddress();
@@ -32,6 +32,17 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
     error Router__CollateralMismatch();
     error Router__InsufficientBalance();
     error Router__InvalidDelta();
+    error Router__ActionNotRecognized();
+
+    bytes32 private constant HASHED_DEPOSIT_FOR = keccak256(abi.encode("routerDepositFor"));
+    bytes32 private constant HASHED_WITHDRAW_COLLATERAL = keccak256(abi.encode("routerWithdrawCollateralFor"));
+    bytes32 private constant HASHED_WITHDRAW_OUTCOME = keccak256(abi.encode("routerWithdrawOutcomeFor"));
+    bytes32 private constant HASHED_MINT = keccak256(abi.encode("routerMintCompleteSets"));
+    bytes32 private constant HASHED_REDEEM = keccak256(abi.encode("routerRedeemCompleteSets"));
+    bytes32 private constant HASHED_SWAP_YES_FOR_NO = keccak256(abi.encode("routerSwapYesForNo"));
+    bytes32 private constant HASHED_SWAP_NO_FOR_YES = keccak256(abi.encode("routerSwapNoForYes"));
+    bytes32 private constant HASHED_ADD_LIQ = keccak256(abi.encode("routerAddLiquidity"));
+    bytes32 private constant HASHED_REMOVE_LIQ = keccak256(abi.encode("routerRemoveLiquidity"));
 
     IERC20 public immutable collateralToken;
 
@@ -54,8 +65,8 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
     event LiquidityAdded(address indexed user, address indexed market, uint256 yesIn, uint256 noIn, uint256 sharesOut);
     event LiquidityRemoved(address indexed user, address indexed market, uint256 sharesIn, uint256 yesOut, uint256 noOut);
 
-    constructor(address collateral, address initialOwner) Ownable(initialOwner) {
-        if (collateral == address(0) || initialOwner == address(0)) revert Router__ZeroAddress();
+    constructor(address collateral, address forwarder, address initialOwner) ReceiverTemplate(forwarder, initialOwner) {
+        if (collateral == address(0)) revert Router__ZeroAddress();
         collateralToken = IERC20(collateral);
     }
 
@@ -66,34 +77,70 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
     }
 
     function depositCollateral(uint256 amount) external nonReentrant {
-        collateralToken.safeTransferFrom(msg.sender, address(this), amount);
-        collateralCredits[msg.sender] += amount;
-        emit Deposited(msg.sender, amount);
+        _depositCollateral(msg.sender, amount);
     }
 
     function withdrawCollateral(uint256 amount) external nonReentrant {
-        uint256 bal = collateralCredits[msg.sender];
-        if (bal < amount) revert Router__InsufficientBalance();
-        collateralCredits[msg.sender] = bal - amount;
-        collateralToken.safeTransfer(msg.sender, amount);
-        emit CollateralWithdrawn(msg.sender, amount);
+        _withdrawCollateral(msg.sender, amount);
     }
 
     function withdrawOutcomeToken(address token, uint256 amount) external nonReentrant {
-        uint256 bal = tokenCredits[msg.sender][token];
-        if (bal < amount) revert Router__InsufficientBalance();
-        tokenCredits[msg.sender][token] = bal - amount;
-        IERC20(token).safeTransfer(msg.sender, amount);
-        emit OutcomeWithdrawn(msg.sender, token, amount);
+        _withdrawOutcomeToken(msg.sender, token, amount);
     }
 
     function mintCompleteSets(address market, uint256 amount) external nonReentrant {
+        _mintCompleteSets(msg.sender, market, amount);
+    }
+
+    function redeemCompleteSets(address market, uint256 amount) external nonReentrant {
+        _redeemCompleteSets(msg.sender, market, amount);
+    }
+
+    function swapYesForNo(address market, uint256 yesIn, uint256 minNoOut) external nonReentrant {
+        _swapYesForNo(msg.sender, market, yesIn, minNoOut);
+    }
+
+    function swapNoForYes(address market, uint256 noIn, uint256 minYesOut) external nonReentrant {
+        _swapNoForYes(msg.sender, market, noIn, minYesOut);
+    }
+
+    function addLiquidity(address market, uint256 yesAmount, uint256 noAmount, uint256 minShares) external nonReentrant {
+        _addLiquidity(msg.sender, market, yesAmount, noAmount, minShares);
+    }
+
+    function removeLiquidity(address market, uint256 shares, uint256 minYesOut, uint256 minNoOut) external nonReentrant {
+        _removeLiquidity(msg.sender, market, shares, minYesOut, minNoOut);
+    }
+
+    function _depositCollateral(address user, uint256 amount) internal {
+        collateralToken.safeTransferFrom(user, address(this), amount);
+        collateralCredits[user] += amount;
+        emit Deposited(user, amount);
+    }
+
+    function _withdrawCollateral(address user, uint256 amount) internal {
+        uint256 bal = collateralCredits[user];
+        if (bal < amount) revert Router__InsufficientBalance();
+        collateralCredits[user] = bal - amount;
+        collateralToken.safeTransfer(user, amount);
+        emit CollateralWithdrawn(user, amount);
+    }
+
+    function _withdrawOutcomeToken(address user, address token, uint256 amount) internal {
+        uint256 bal = tokenCredits[user][token];
+        if (bal < amount) revert Router__InsufficientBalance();
+        tokenCredits[user][token] = bal - amount;
+        IERC20(token).safeTransfer(user, amount);
+        emit OutcomeWithdrawn(user, token, amount);
+    }
+
+    function _mintCompleteSets(address user, address market, uint256 amount) internal {
         _validateMarket(market);
         _ensureCollateralMatch(market);
 
-        uint256 userCollateral = collateralCredits[msg.sender];
+        uint256 userCollateral = collateralCredits[user];
         if (userCollateral < amount) revert Router__InsufficientBalance();
-        collateralCredits[msg.sender] = userCollateral - amount;
+        collateralCredits[user] = userCollateral - amount;
 
         address yes = IPredictionMarketLike(market).yesToken();
         address no = IPredictionMarketLike(market).noToken();
@@ -108,22 +155,22 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
         uint256 yesDelta = yesAfter - yesBefore;
         uint256 noDelta = noAfter - noBefore;
 
-        tokenCredits[msg.sender][yes] += yesDelta;
-        tokenCredits[msg.sender][no] += noDelta;
+        tokenCredits[user][yes] += yesDelta;
+        tokenCredits[user][no] += noDelta;
 
-        emit CompleteSetsMinted(msg.sender, market, amount, yesDelta, noDelta);
+        emit CompleteSetsMinted(user, market, amount, yesDelta, noDelta);
     }
 
-    function redeemCompleteSets(address market, uint256 amount) external nonReentrant {
+    function _redeemCompleteSets(address user, address market, uint256 amount) internal {
         _validateMarket(market);
         address yes = IPredictionMarketLike(market).yesToken();
         address no = IPredictionMarketLike(market).noToken();
 
-        uint256 yesBal = tokenCredits[msg.sender][yes];
-        uint256 noBal = tokenCredits[msg.sender][no];
+        uint256 yesBal = tokenCredits[user][yes];
+        uint256 noBal = tokenCredits[user][no];
         if (yesBal < amount || noBal < amount) revert Router__InsufficientBalance();
-        tokenCredits[msg.sender][yes] = yesBal - amount;
-        tokenCredits[msg.sender][no] = noBal - amount;
+        tokenCredits[user][yes] = yesBal - amount;
+        tokenCredits[user][no] = noBal - amount;
 
         uint256 collateralBefore = collateralToken.balanceOf(address(this));
         _ensureAllowance(IERC20(yes), market, amount);
@@ -133,19 +180,19 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
 
         if (collateralAfter < collateralBefore) revert Router__InvalidDelta();
         uint256 collateralDelta = collateralAfter - collateralBefore;
-        collateralCredits[msg.sender] += collateralDelta;
+        collateralCredits[user] += collateralDelta;
 
-        emit CompleteSetsRedeemed(msg.sender, market, amount, collateralDelta);
+        emit CompleteSetsRedeemed(user, market, amount, collateralDelta);
     }
 
-    function swapYesForNo(address market, uint256 yesIn, uint256 minNoOut) external nonReentrant {
+    function _swapYesForNo(address user, address market, uint256 yesIn, uint256 minNoOut) internal {
         _validateMarket(market);
         address yes = IPredictionMarketLike(market).yesToken();
         address no = IPredictionMarketLike(market).noToken();
 
-        uint256 yesBal = tokenCredits[msg.sender][yes];
+        uint256 yesBal = tokenCredits[user][yes];
         if (yesBal < yesIn) revert Router__InsufficientBalance();
-        tokenCredits[msg.sender][yes] = yesBal - yesIn;
+        tokenCredits[user][yes] = yesBal - yesIn;
 
         uint256 noBefore = IERC20(no).balanceOf(address(this));
         _ensureAllowance(IERC20(yes), market, yesIn);
@@ -154,19 +201,19 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
 
         if (noAfter < noBefore) revert Router__InvalidDelta();
         uint256 noOut = noAfter - noBefore;
-        tokenCredits[msg.sender][no] += noOut;
+        tokenCredits[user][no] += noOut;
 
-        emit SwappedYesForNo(msg.sender, market, yesIn, noOut);
+        emit SwappedYesForNo(user, market, yesIn, noOut);
     }
 
-    function swapNoForYes(address market, uint256 noIn, uint256 minYesOut) external nonReentrant {
+    function _swapNoForYes(address user, address market, uint256 noIn, uint256 minYesOut) internal {
         _validateMarket(market);
         address yes = IPredictionMarketLike(market).yesToken();
         address no = IPredictionMarketLike(market).noToken();
 
-        uint256 noBal = tokenCredits[msg.sender][no];
+        uint256 noBal = tokenCredits[user][no];
         if (noBal < noIn) revert Router__InsufficientBalance();
-        tokenCredits[msg.sender][no] = noBal - noIn;
+        tokenCredits[user][no] = noBal - noIn;
 
         uint256 yesBefore = IERC20(yes).balanceOf(address(this));
         _ensureAllowance(IERC20(no), market, noIn);
@@ -175,21 +222,23 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
 
         if (yesAfter < yesBefore) revert Router__InvalidDelta();
         uint256 yesOut = yesAfter - yesBefore;
-        tokenCredits[msg.sender][yes] += yesOut;
+        tokenCredits[user][yes] += yesOut;
 
-        emit SwappedNoForYes(msg.sender, market, noIn, yesOut);
+        emit SwappedNoForYes(user, market, noIn, yesOut);
     }
 
-    function addLiquidity(address market, uint256 yesAmount, uint256 noAmount, uint256 minShares) external nonReentrant {
+    function _addLiquidity(address user, address market, uint256 yesAmount, uint256 noAmount, uint256 minShares)
+        internal
+    {
         _validateMarket(market);
         address yes = IPredictionMarketLike(market).yesToken();
         address no = IPredictionMarketLike(market).noToken();
 
-        uint256 yesBal = tokenCredits[msg.sender][yes];
-        uint256 noBal = tokenCredits[msg.sender][no];
+        uint256 yesBal = tokenCredits[user][yes];
+        uint256 noBal = tokenCredits[user][no];
         if (yesBal < yesAmount || noBal < noAmount) revert Router__InsufficientBalance();
-        tokenCredits[msg.sender][yes] = yesBal - yesAmount;
-        tokenCredits[msg.sender][no] = noBal - noAmount;
+        tokenCredits[user][yes] = yesBal - yesAmount;
+        tokenCredits[user][no] = noBal - noAmount;
 
         uint256 sharesBefore = IPredictionMarketLike(market).lpShares(address(this));
         _ensureAllowance(IERC20(yes), market, yesAmount);
@@ -199,16 +248,18 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
         if (sharesAfter < sharesBefore) revert Router__InvalidDelta();
 
         uint256 sharesOut = sharesAfter - sharesBefore;
-        lpShareCredits[msg.sender][market] += sharesOut;
+        lpShareCredits[user][market] += sharesOut;
 
-        emit LiquidityAdded(msg.sender, market, yesAmount, noAmount, sharesOut);
+        emit LiquidityAdded(user, market, yesAmount, noAmount, sharesOut);
     }
 
-    function removeLiquidity(address market, uint256 shares, uint256 minYesOut, uint256 minNoOut) external nonReentrant {
+    function _removeLiquidity(address user, address market, uint256 shares, uint256 minYesOut, uint256 minNoOut)
+        internal
+    {
         _validateMarket(market);
-        uint256 userShares = lpShareCredits[msg.sender][market];
+        uint256 userShares = lpShareCredits[user][market];
         if (userShares < shares) revert Router__InsufficientBalance();
-        lpShareCredits[msg.sender][market] = userShares - shares;
+        lpShareCredits[user][market] = userShares - shares;
 
         address yes = IPredictionMarketLike(market).yesToken();
         address no = IPredictionMarketLike(market).noToken();
@@ -223,10 +274,50 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
 
         uint256 yesOut = yesAfter - yesBefore;
         uint256 noOut = noAfter - noBefore;
-        tokenCredits[msg.sender][yes] += yesOut;
-        tokenCredits[msg.sender][no] += noOut;
+        tokenCredits[user][yes] += yesOut;
+        tokenCredits[user][no] += noOut;
 
-        emit LiquidityRemoved(msg.sender, market, shares, yesOut, noOut);
+        emit LiquidityRemoved(user, market, shares, yesOut, noOut);
+    }
+
+    function _processReport(bytes calldata report) internal override nonReentrant {
+        (string memory actionType, bytes memory payload) = abi.decode(report, (string, bytes));
+        bytes32 actionTypeHash = keccak256(abi.encode(actionType));
+
+        if (actionTypeHash == HASHED_DEPOSIT_FOR) {
+            (address user, uint256 amount) = abi.decode(payload, (address, uint256));
+            _depositCollateral(user, amount);
+        } else if (actionTypeHash == HASHED_WITHDRAW_COLLATERAL) {
+            (address user, uint256 amount) = abi.decode(payload, (address, uint256));
+            _withdrawCollateral(user, amount);
+        } else if (actionTypeHash == HASHED_WITHDRAW_OUTCOME) {
+            (address user, address token, uint256 amount) = abi.decode(payload, (address, address, uint256));
+            _withdrawOutcomeToken(user, token, amount);
+        } else if (actionTypeHash == HASHED_MINT) {
+            (address user, address market, uint256 amount) = abi.decode(payload, (address, address, uint256));
+            _mintCompleteSets(user, market, amount);
+        } else if (actionTypeHash == HASHED_REDEEM) {
+            (address user, address market, uint256 amount) = abi.decode(payload, (address, address, uint256));
+            _redeemCompleteSets(user, market, amount);
+        } else if (actionTypeHash == HASHED_SWAP_YES_FOR_NO) {
+            (address user, address market, uint256 yesIn, uint256 minNoOut) =
+                abi.decode(payload, (address, address, uint256, uint256));
+            _swapYesForNo(user, market, yesIn, minNoOut);
+        } else if (actionTypeHash == HASHED_SWAP_NO_FOR_YES) {
+            (address user, address market, uint256 noIn, uint256 minYesOut) =
+                abi.decode(payload, (address, address, uint256, uint256));
+            _swapNoForYes(user, market, noIn, minYesOut);
+        } else if (actionTypeHash == HASHED_ADD_LIQ) {
+            (address user, address market, uint256 yesAmount, uint256 noAmount, uint256 minShares) =
+                abi.decode(payload, (address, address, uint256, uint256, uint256));
+            _addLiquidity(user, market, yesAmount, noAmount, minShares);
+        } else if (actionTypeHash == HASHED_REMOVE_LIQ) {
+            (address user, address market, uint256 shares, uint256 minYesOut, uint256 minNoOut) =
+                abi.decode(payload, (address, address, uint256, uint256, uint256));
+            _removeLiquidity(user, market, shares, minYesOut, minNoOut);
+        } else {
+            revert Router__ActionNotRecognized();
+        }
     }
 
     function _validateMarket(address market) internal view {
@@ -246,4 +337,3 @@ contract PredictionMarketRouterVault is Ownable, ReentrancyGuard {
         token.safeIncreaseAllowance(spender, increase);
     }
 }
-
